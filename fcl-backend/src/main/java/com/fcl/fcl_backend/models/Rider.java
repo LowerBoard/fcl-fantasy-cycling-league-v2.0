692ea64f441d6f0ce83b2787c3499b59ca92e871
cmlorbert <cmlorbert@gmail.com>
2026-03-02 03:11:59 -0600

    add Rider entity model

---
+++ fcl-backend/src/main/java/com/fcl/fcl_backend/models/Rider.java
@@ -0,0 +1,60 @@
+package com.fcl.fcl_backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // tells JPA that this class is an entity and should be mapped to a database table
+public class Rider {
+    @Id // tells JPA that this field is the primary key of the entity
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // tells JPA to automatically generate the value of this field using the database's identity column feature
+    private Long id;
+    private String name;
+    private String proTeam;
+    private int points;
+
+    public Rider() {
+    }
+
+    public Rider(String name, String proTeam, int points) {
+        this.name = name;
+        this.proTeam = proTeam;
+        this.points = points;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProTeam() {
+        return proTeam;
+    }
+
+    public void setProTeam(String proTeam) {
+        this.proTeam = proTeam;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
+
+
