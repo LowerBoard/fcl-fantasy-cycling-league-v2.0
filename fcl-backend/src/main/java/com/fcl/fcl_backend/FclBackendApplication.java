08f677e77c2878d1025cf379679706ba08e50c2a
cmlorbert <cmlorbert@gmail.com>
2026-03-02 02:19:37 -0600

    fcl_db connected

---
+++ fcl-backend/src/main/java/com/fcl/fcl_backend/FclBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fcl.fcl_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FclBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FclBackendApplication.class, args);
+	}
+
+}
---
+++ fcl-backend/src/test/java/com/fcl/fcl_backend/FclBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcl.fcl_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FclBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
