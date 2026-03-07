5fb248c7e69ac5bbd534bbd7cd513d2dd72519ab
cmlorbert <cmlorbert@gmail.com>
2026-03-06 22:26:10 -0600

    Roster model added

---
+++ fcl-backend/src/main/java/com/fcl/fcl_backend/models/Roster.java
@@ -0,0 +1,71 @@
+package com.fcl.fcl_backend.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "rosters")
+public class Roster {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "race_id", nullable = false)
+    private Race race;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "user_team_id", nullable = false)
+    private UserTeam userTeam;
+
+    @ManyToMany
+    @JoinTable(
+            name = "roster_riders",
+            joinColumns = @JoinColumn(name = "roster_id"),
+            inverseJoinColumns = @JoinColumn(name = "rider_id")
+    )
+    private List<Rider> riders;
+
+    public Roster() {}
+
+    public Roster(Race race, UserTeam userTeam) {
+        this.race = race;
+        this.userTeam = userTeam;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Race getRace() {
+        return race;
+    }
+
+    public void setRace(Race race) {
+        this.race = race;
+    }
+
+    public UserTeam getUserTeam() {
+        return userTeam;
+    }
+
+    public void setUserTeam(UserTeam userTeam) {
+        this.userTeam = userTeam;
+    }
+
+    public List<Rider> getRiders() {
+        return riders;
+    }
+
+    public void setRiders(List<Rider> riders) {
+        this.riders = riders;
+    }
+}
